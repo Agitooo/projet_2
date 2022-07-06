79184ea4b1a1cb3bb2509f0655cfd1005be67628
Vincent <>
2022-07-06 20:50:48 +0200

    ajout exo 2-3

--- script.py
+++ script.py
@@ -1,15 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
-# site a scrapper
 url = "http://books.toscrape.com/"
-response = requests.get(url)
-page = response.content
-soup = BeautifulSoup(page, "html.parser")
 
-# Ne fontionne que sur la page principale
-def get_all_categorie():
+
+def get_menu():
+    return input("Veuillez selectionner votre action parmis les choix suivants \n "
+                 "1    - Récupérer les détails d'un livre par son url \n "
+                 "2    - Récupérer les livres d'une catégorie selectionnée \n "
+                 "3    - Récupération de l'integralité des livres du site \n "
+                 "exit - Quitter\n "
+                 " ").strip().lower()
+
+
+def get_select_url():
+    return input("Veuillez saisir l'url du livre : ").strip().lower()
+
+
+# On affiche l'input de selection de la catégorie
+def input_select_cat():
+    return input("Veuillez saisir la catégorie à récuperer : ").strip().lower()
+
+
+def with_picture():
+    text_picture = "Voulez-vous télécharger les images (y/n)? ".strip().lower()
+    with_picture_choice = input(text_picture)
+    while not (with_picture_choice in allowed_with_picture):
+        print("Le choix n'est pas valide")
+        with_picture_choice = input(text_picture)
+
+    if with_picture_choice == "y":
+        with_picture = True
+    else:
+        with_picture = False
+
+    return with_picture
+
+
+# On récupère la clé de la valeur recherchée dans le dictionnaire si elle existe, sinon none
+def key_in_dict(dictionary, key, search):
+    return next((i for i, item in enumerate(dictionary) if item[key] == search), None)
+
+
+
+
+def get_dict_all_category():
+    # site a scrapper
+    response = requests.get(url)
+    page = response.content
+    soup = BeautifulSoup(page, "html.parser")
+
     # On récupère toutes les catégories de la page principale
     toutes_categorie_ul = soup.find_all("ul", class_="nav-list")
 
@@ -18,15 +60,11 @@ def get_all_categorie():
     for bloc_categorie in toutes_categorie_ul:
         all_a_cat = bloc_categorie.find_all("a")
         for a_info in all_a_cat:
-            categorie.append({'nom': a_info.get_text().strip(), 'url': a_info.get('href')})
+            categorie.append({'nom': a_info.get_text().strip().lower(), 'url': a_info.get('href')})
     # On supprime la 1ere catégorie qui est un titre
     categorie.pop(0)
     # categorie = Tableau des catégories + url associées
     return categorie
-
-
-# tab_categotie = get_all_categorie()
-# print(tab_categotie)
 
 
 # charger la donnée dans un fichier csv
@@ -35,17 +73,19 @@ def charger_donnees(
         en_tete,
         detail_livre
 ):
-    with open(nom_fichier, 'w') as fichier_csv:
+    with open(nom_fichier, 'w', encoding="utf8") as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=',')
-        writer.writerow(en_tete)
-        writer.writerow(
+        # Si le fichier est vide, alors on met l'entete
+        if os.path.getsize(nom_fichier) == 0:
+            writer.writerow(en_tete)
+        writer.writerows(
             [
-                (','.join(detail_livre))
+               detail_livre
             ]
         )
 
 
-def get_detail_livre(url_detail_livre):
+def get_detail_livre(url_detail_livre, with_picture=False, cat_dir=""):
 
     response_detail_livre = requests.get(url_detail_livre)
     page_detail_livre = response_detail_livre.content
@@ -75,13 +115,64 @@ def get_detail_livre(url_detail_livre):
     # donc on les retire et on concatène avec l'url du site pour avoir l'url complete
     image_url = url + soup_detail_livre.find('img').get('src')[6:]
 
+    if with_picture:
+        if cat_dir != "":
+            name_picture = cat_dir + "/" + title.replace(" ", "_")
+        else:
+            name_picture = title.replace(" ", "_")
+        f = open(name_picture + ".jpg", "wb")
+        picture = requests.get(image_url)
+        f.write(picture.content)
+        f.close()
+
     data_return = [
         product_page_url, universal_product_code, price_including_tax, price_excluding_tax, number_available,
         title, product_description, category, review_rating, image_url
     ]
-
     return data_return
 
+
+def get_list_book_by_category(category_url):
+    reponse_category_par_page = requests.get(category_url)
+    page_category_par_page = reponse_category_par_page.content
+    soup_category_par_page = BeautifulSoup(page_category_par_page, "html.parser")
+
+    all_link_livre = soup_category_par_page.find_all("div", class_="image_container")
+
+    have_pagination = soup_category_par_page.find_all("li", class_="next")
+    if have_pagination:
+        for link_pagination in have_pagination:
+            a_pagination = link_pagination.find_all("a")
+            url_pagination = a_pagination[0].get('href')
+            # print(url_pagination)
+
+    return all_link_livre
+
+
+def get_all_book_detail(category_dict, with_picture=False):
+
+    for category in category_dict:
+        category_url = url + category['url']
+
+        all_link_livre = get_list_book_by_category(category_url)
+        cat_dir = category["nom"].strip().replace(" ", "_").lower()
+        if not os.path.exists(cat_dir):
+            os.makedirs(cat_dir)
+        with open(cat_dir + '/' + category["nom"] + ".csv", 'w', encoding="utf8") as fichier_csv:
+            writer = csv.writer(fichier_csv, delimiter=',')
+            # Si le fichier est vide, alors on met l'entete
+            if os.path.getsize(cat_dir + '/' + category["nom"] + ".csv") == 0:
+                writer.writerow(en_tete)
+
+            for bloc_link_livre in all_link_livre:
+                all_a_livre = bloc_link_livre.find_all("a")
+
+                for link_livre in all_a_livre:
+                    link = url + 'catalogue/' + link_livre.get('href')[9:]
+                    writer.writerows([get_detail_livre(link, with_picture, cat_dir)])
+
+
+url_choice = ""
 
 # entete du fichier CSV
 en_tete = [
@@ -97,12 +188,60 @@ en_tete = [
     "image_url"
 ]
 
-# Url du détail du 1er livre
-url_detail_livre = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-detail_livre = get_detail_livre(url_detail_livre)
+# On gère le choix du menu principal
+allowed_menu_choice = ["1", "2", "3", "exit"]
+allowed_with_picture = ["y", "n"]
 
-charger_donnees(
-    "partie_1_detail_livre.csv",
-    en_tete,
-    detail_livre
-)
+menu_choice = get_menu()
+index_cat_in_dict = None
+
+# Tant que le choix est invalide, on demande de ressaisir
+while not(menu_choice in allowed_menu_choice):
+    print("Le choix n'est pas valide")
+    menu_choice = get_menu()
+
+if menu_choice == "1":
+    url_choice = get_select_url()
+    test_url = requests.get(url_choice)
+    while test_url.status_code != 200:
+        print("URL invalide")
+        url_choice = get_select_url()
+        test_url = requests.get(url_choice)
+
+    with_picture = with_picture()
+
+    detail_livre = get_detail_livre(url_choice, with_picture, "")
+    charger_donnees(
+        "detail_livre.csv",
+        en_tete,
+        detail_livre
+    )
+
+elif menu_choice == "2":
+    # On récupère un dictionnaire de toutes les catégories
+    all_category = get_dict_all_category()
+    # On affiche l'input de selection de la catégorie
+    cat_select = input_select_cat()
+    # On récupère la clé de la catégorie recherchée dans le dictionnaire de toutes les catégories, sinon None
+    index_cat_in_dict = key_in_dict(all_category, "nom", cat_select)
+
+    # Tant qu'on a pas de clé trouvée, on redemande de saisir la catégorie, jusqu'a en avoir une valide
+    while not index_cat_in_dict:
+        print("La catégorie saisie n'éxiste pas")
+        cat_select = input_select_cat()
+        index_cat_in_dict = key_in_dict(all_category, "nom", cat_select)
+
+    with_picture = with_picture()
+
+    get_all_book_detail([all_category[index_cat_in_dict]], with_picture)
+
+elif menu_choice == "3":
+    # On récupère un dictionnaire de toutes les catégories
+    all_category = get_dict_all_category()
+
+    with_picture = with_picture()
+
+    get_all_book_detail(all_category, with_picture)
+
+elif menu_choice == "exit":
+    exit()
